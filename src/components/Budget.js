9023c1a53241ecd42bfdf9368862f360b62495c6
RehanGeorge <rehan.george@gmail.com>
2024-02-18 17:37:26 +0530

    Add Set Budget functionality

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -2,20 +2,30 @@ import React, { useContext, useState } from "react";
 import { AppContext } from "../context/AppContext";
 
 const Budget = () => {
-  const { budget } = useContext(AppContext);
+  const { budget, dispatch } = useContext(AppContext);
   const [newBudget, setNewBudget] = useState(budget);
+
   const handleBudgetChange = (event) => {
     setNewBudget(event.target.value);
   };
+
+  const updateBudget = () => {
+    dispatch({
+      type: "SET_BUDGET",
+      payload: newBudget,
+    });
+  };
   return (
     <div className="alert alert-secondary">
-      <span>Budget: £{budget}</span>
+      <span>Budget: £</span>
       <input
         type="number"
         step="10"
+        min="0"
         value={newBudget}
         onChange={handleBudgetChange}
       ></input>
+      <button onClick={updateBudget}>Update Budget</button>
     </div>
   );
 };
